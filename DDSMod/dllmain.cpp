d8e5a2e37c431e6936c7fd21591ec84e7cf57254
meiskam <meiskam@users.noreply.github.com>
2020-04-22 03:38:25 -0400

    blank main func

--- DDSMod/dllmain.cpp
+++ DDSMod/dllmain.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include "mem.h"
 
-//https://guidedhacking.com/threads/how-to-hack-any-game-first-internal-hack-dll-tutorial.12142/
-
 DWORD WINAPI HackThread(HMODULE hModule)
 {
 	//Create Console
@@ -12,14 +10,14 @@ DWORD WINAPI HackThread(HMODULE hModule)
 	FILE* f;
 	freopen_s(&f, "CONOUT$", "w", stdout);
 
-	std::cout << "OG for a fee, stay sippin' fam\n";
+	std::cout << "Loaded\n";
 
-	uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
+	uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"DrugDealerSimulator-Win64-Shipping.exe");
 
 	//calling it with NULL also gives you the address of the .exe module
 	moduleBase = (uintptr_t)GetModuleHandle(NULL);
 
-	bool bHealth = false, bAmmo = false, bRecoil = false;
+	//bool bHealth = false, bAmmo = false, bRecoil = false;
 
 	while (true)
 	{
@@ -28,62 +26,62 @@ DWORD WINAPI HackThread(HMODULE hModule)
 			break;
 		}
 
-		if (GetAsyncKeyState(VK_NUMPAD1) & 1)
-			bHealth = !bHealth;
+		//if (GetAsyncKeyState(VK_NUMPAD1) & 1)
+		//	bHealth = !bHealth;
 
-		if (GetAsyncKeyState(VK_NUMPAD2) & 1)
-		{
-			bAmmo = !bAmmo;
-		}
+		//if (GetAsyncKeyState(VK_NUMPAD2) & 1)
+		//{
+		//	bAmmo = !bAmmo;
+		//}
 
-		//no recoil NOP
-		if (GetAsyncKeyState(VK_NUMPAD3) & 1)
-		{
-			bRecoil = !bRecoil;
+		////no recoil NOP
+		//if (GetAsyncKeyState(VK_NUMPAD3) & 1)
+		//{
+		//	bRecoil = !bRecoil;
 
-			if (bRecoil)
-			{
-				mem::Nop((BYTE*)(moduleBase + 0x63786), 10);
-			}
+		//	if (bRecoil)
+		//	{
+		//		mem::Nop((BYTE*)(moduleBase + 0x63786), 10);
+		//	}
 
-			else
-			{
-				//50 8D 4C 24 1C 51 8B CE FF D2 the original stack setup and call
-				mem::Patch((BYTE*)(moduleBase + 0x63786), (BYTE*)"\x50\x8D\x4C\x24\x1C\x51\x8B\xCE\xFF\xD2", 10);
-			}
-		}
+		//	else
+		//	{
+		//		//50 8D 4C 24 1C 51 8B CE FF D2 the original stack setup and call
+		//		mem::Patch((BYTE*)(moduleBase + 0x63786), (BYTE*)"\x50\x8D\x4C\x24\x1C\x51\x8B\xCE\xFF\xD2", 10);
+		//	}
+		//}
 
-		//need to use uintptr_t for pointer arithmetic later
-		uintptr_t* localPlayerPtr = (uintptr_t*)(moduleBase + 0x10F4F4);
+		////need to use uintptr_t for pointer arithmetic later
+		//uintptr_t* localPlayerPtr = (uintptr_t*)(moduleBase + 0x10F4F4);
 
-		//continuous writes / freeze
+		////continuous writes / freeze
 
-		if (localPlayerPtr)
-		{
-			if (bHealth)
-			{
+		//if (localPlayerPtr)
+		//{
+		//	if (bHealth)
+		//	{
 
-				//*localPlayerPtr = derference the pointer, to get the localPlayerAddr
-				// add 0xF8 to get health address
-				//cast to an int pointer, this pointer now points to the health address
-				//derference it and assign the value 1337 to the health variable it points to
-				*(int*)(*localPlayerPtr + 0xF8) = 1337;
-			}
+		//		//*localPlayerPtr = derference the pointer, to get the localPlayerAddr
+		//		// add 0xF8 to get health address
+		//		//cast to an int pointer, this pointer now points to the health address
+		//		//derference it and assign the value 1337 to the health variable it points to
+		//		*(int*)(*localPlayerPtr + 0xF8) = 1337;
+		//	}
 
-			if (bAmmo)
-			{
-				//We aren't external now, we can now efficiently calculate all pointers dynamically
-				//before we only resolved pointers when needed for efficiency reasons
-				//we are executing internally, we can calculate everything when needed
-				uintptr_t ammoAddr = mem::FindDMAAddy(moduleBase + 0x10F4F4, { 0x374, 0x14, 0x0 });
-				int* ammo = (int*)ammoAddr;
-				*ammo = 1337;
+		//	if (bAmmo)
+		//	{
+		//		//We aren't external now, we can now efficiently calculate all pointers dynamically
+		//		//before we only resolved pointers when needed for efficiency reasons
+		//		//we are executing internally, we can calculate everything when needed
+		//		uintptr_t ammoAddr = mem::FindDMAAddy(moduleBase + 0x10F4F4, { 0x374, 0x14, 0x0 });
+		//		int* ammo = (int*)ammoAddr;
+		//		*ammo = 1337;
 
-				//or just
-				*(int*)mem::FindDMAAddy(moduleBase + 0x10F4F4, { 0x374, 0x14, 0x0 }) = 1337;
-			}
+		//		//or just
+		//		*(int*)mem::FindDMAAddy(moduleBase + 0x10F4F4, { 0x374, 0x14, 0x0 }) = 1337;
+		//	}
 
-		}
+		//}
 		Sleep(5);
 	}
 
